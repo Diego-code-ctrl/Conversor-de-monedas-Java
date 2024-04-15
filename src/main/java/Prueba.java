167e2807159b1fdd886941050ef5d04e5f46bf61
Diego Bece <diebecmag@outlook.com>
2024-04-14 20:59:04 -0600

    Intercambio de moneda (Exitosa)

--- src/main/java/Prueba.java
+++ src/main/java/Prueba.java
@@ -1,4 +1,6 @@
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -10,15 +12,28 @@ import java.net.http.HttpResponse;
  * @author diegobecerril
  */
 public class Prueba {
-    
+
     private static final String apiKey = "https://v6.exchangerate-api.com/v6/1907c772ff6d84acfefcfd96/latest/USD";
-    
+
     public static void main(String[] args) throws IOException, InterruptedException {
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder().uri(URI.create(apiKey)).build();
-        
+
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        String jsonString = response.body().toString();
         
-        System.out.println(response.body());
+        //System.out.println("jsonString = " + jsonString);
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        JsonNode node = mapper.readTree(jsonString);
+        var conversion_rates = node.get("conversion_rates");
+        
+        conversion_rates.fields().forEachRemaining(entry -> {
+            String key = entry.getKey();
+            double rate = entry.getValue().asDouble();
+            System.out.println(key+ " : " + rate);
+        });
     }
 }
